abbf40469fae623f0b43c84abf3715ed68a59abb
drmidn1ght <ana.drmidnight@gmail.com>
2023-10-12 18:44:31 -0300

    feat Criando header

---
+++ to-do-list/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header>
+      <h1
+        aria-label="Lista de Tarefas"
+        aria-hidden="false"
+        role="heading"
+      >
+        Lista de tarefas
+      </h1>
+    </header>
+  );
+};
+
+/* Cabeçalho otimizado para leitores de tela. 
+    - area-label: cria um texto alternativo;
+        - area-hidden: Oculta algum componente de leitores de tela, por exemplo, efeitos visuais. Neste caso, está definido como false;
+            - role: Define o papel do componente. */
