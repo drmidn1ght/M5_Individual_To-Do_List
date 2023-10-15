451a7d5543012bec10c4412128a38442b230872f
drmidn1ght <ana.drmidnight@gmail.com>
2023-10-14 21:50:17 -0300

    fix Corrigindo importações e erros de sintaxe

--- to-do-list/src/components/ListaItem.jsx
+++ to-do-list/src/components/ListaItem.jsx
@@ -1,12 +1,5 @@
 
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import styled from "styled-components";
-
-import Header from "./components/Header.jsx";
-import ListaTarefas from "./components/ListaTarefas.jsx";
-import Footer from "./components/Footer.jsx"; 
 
 import Button from "./components/Button"; 
 
--- to-do-list/src/pages/LandingPage/LandingPage.jsx
+++ to-do-list/src/pages/LandingPage/LandingPage.jsx
@@ -1,10 +1,21 @@
 
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import styled from "styled-components";
 
 import Header from "./components/Header.jsx";
 import ListaTarefas from "./components/ListaTarefas.jsx";
 import ListaItem from "./components/ListaItem.jsx";
-import Footer from "./components/Footer.jsx";
+import Footer from "./components/Footer.jsx"; 
+
+
+const LandingPage = () => {
+  return (
+    <div>
+    <Header/>
+    <ListaTarefas/>
+    <ListaItem/>
+    <Footer/>
+    </div>
+  )
+}
+
+export default LandingPage
